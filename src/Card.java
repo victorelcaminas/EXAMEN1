66721538abf66362dfc16703ba87423154838593
valonso <vmalonso@gmail.com>
2021-02-02 18:38:14 +0100

    Exercise b)

--- src/Card.java
+++ src/Card.java
@@ -11,4 +11,41 @@ public class Card {
         this.number = number;
         this.suit = suit;
     }
+
+    @Override
+    public String toString() {
+        String numberStr = getNumberStr();
+        String suitStr = getSuitStr();
+        return numberStr + " de " + suitStr;
+    }
+
+    private String getNumberStr() {
+        String s;
+        switch (number) {
+            case 10: s = "sota";
+                        break;
+            case 11: s = "caballo";
+                        break;
+            case 12: s = "rey";
+                        break;
+            default: s = "" + number;
+        }
+        return s;
+    }
+
+    private String getSuitStr() {
+        String s = "";
+        switch (suit) {
+            case OROS: s = "oros";
+                    break;
+            case COPAS: s = "copas";
+                    break;
+            case ESPADAS: s = "espadas";
+                    break;
+            case BASTOS: s = "bastos";
+                    break;
+        }
+        return s;
+    }
+
 }
---
+++ src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        Card c = new Card(1,1);
+        System.out.println(c);
+    }
+}
